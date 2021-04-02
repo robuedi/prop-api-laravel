919a5e6ca855f00df312fe96d9582e03ff142d1d
Eduard Robu <edi_cristi3@yahoo.com>
2021-04-02 11:40:53 +0100

    routes updates

--- routes/api.php
+++ routes/api.php
@@ -41,33 +41,36 @@ Route::prefix('v1')->group(function (){
 
     //properties
     Route::get('/properties', [PropertiesController::class, 'index']);
-    Route::post('/properties', [PropertiesController::class, 'store']);
     Route::get('/properties/{property}', [PropertiesController::class, 'show']);
 
     //properties address
     Route::get('/properties/{property}/address', [PropertiesController::class, 'showPropertyAddress']);
 
-    Route::post('/annual-salaries', [UserAnnualSalariesController::class, 'store']);
-    Route::get('/annual-salaries/current-user', [UserAnnualSalariesController::class, 'showCurrentUser']);
-
-    Route::post('/user-rent', [UserRentsController::class, 'store']);
-    Route::get('/user-rent/current-user', [UserRentsController::class, 'showCurrentUser']);
-
-    Route::post('/user-employment', [UserEmploymentController::class, 'store']);
-    Route::get('/user-employment/current-user', [UserEmploymentController::class, 'showCurrentUser']);
-
-    Route::post('/user-savings', [UserSavingsController::class, 'store']);
-    Route::get('/user-savings/current-user', [UserSavingsController::class, 'showCurrentUser']);
-
-    Route::post('/user-address', [UserAddressController::class, 'store']);
-    Route::get('/user-address/current-user', [UserAddressController::class, 'showCurrentUser']);
-
-    Route::post('/agencies', [AgenciesController::class, 'store']);
-    Route::get('/agencies/current-user', [AgenciesController::class, 'showCurrentUser']);
     Route::get('/agencies/{agency}/address', [AgencyAddressesController::class, 'showForAgency']);
-    Route::post('/agencies/{agency}/address', [AgencyAddressesController::class, 'storeForAgency']);
 
-    Route::get('/users/check-profile-completed', [UsersController::class, 'checkUserProfileComplete']);
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::post('/properties', [PropertiesController::class, 'store']);
 
+        Route::post('/annual-salaries', [UserAnnualSalariesController::class, 'store']);
+        Route::get('/annual-salaries/current-user', [UserAnnualSalariesController::class, 'showCurrentUser']);
+
+        Route::post('/user-rent', [UserRentsController::class, 'store']);
+        Route::get('/user-rent/current-user', [UserRentsController::class, 'showCurrentUser']);
+
+        Route::post('/user-employment', [UserEmploymentController::class, 'store']);
+        Route::get('/user-employment/current-user', [UserEmploymentController::class, 'showCurrentUser']);
+
+        Route::post('/user-savings', [UserSavingsController::class, 'store']);
+        Route::get('/user-savings/current-user', [UserSavingsController::class, 'showCurrentUser']);
+
+        Route::post('/user-address', [UserAddressController::class, 'store']);
+        Route::get('/user-address/current-user', [UserAddressController::class, 'showCurrentUser']);
+
+        Route::post('/agencies', [AgenciesController::class, 'store']);
+        Route::get('/agencies/current-user', [AgenciesController::class, 'showCurrentUser']);
+        Route::post('/agencies/{agency}/address', [AgencyAddressesController::class, 'storeForAgency']);
+
+        Route::get('/users/check-profile-completed', [UsersController::class, 'checkUserProfileComplete']);
+    });
 });
 
