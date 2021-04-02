3b52d481ebcbf589470e78f4993574e6df4ce707
Eduard Robu <edi_cristi3@yahoo.com>
2021-04-02 12:58:10 +0100

    auth, routes updates

--- app/Http/Controllers/Api/v1/UserAddressController.php
+++ app/Http/Controllers/Api/v1/UserAddressController.php
@@ -4,7 +4,10 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\v1\UserAddressShowRequest;
+use App\Http\Requests\v1\UserAddressStoreRequest;
 use App\Http\Resources\v1\UserAddressResource;
+use App\Models\User;
 use App\Repositories\UserAddressRepositoryInterface;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -18,12 +21,12 @@ class UserAddressController extends Controller
         $this->user_address_repository = $user_address_repository;
     }
 
-    public function storeForUser(Request $request)
+    public function storeForUser(User $user, UserAddressStoreRequest $request)
     {
         //make property for user
         return UserAddressResource::make(
             $this->user_address_repository->create(
-                auth()->user()->id,
+                $user->id,
                 $request->get('city_id'),
                 $request->get('address_line'),
                 $request->get('postcode')
@@ -31,11 +34,11 @@ class UserAddressController extends Controller
         )->response()->setStatusCode(Response::HTTP_CREATED);
     }
 
-    public function showForUser()
+    public function showForUser(User $user, UserAddressShowRequest $request)
     {
         //make property for user
         return UserAddressResource::make(
-            $this->user_address_repository->getFirstByUserId(auth()->user()->id)
+            $this->user_address_repository->getFirstByUserId($user->id)
         )->response()->setStatusCode(Response::HTTP_CREATED);
     }
 }
